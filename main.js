e52448b3f39809a2418c6adfb021a0c9cccd6b6d
GabrielZirk <gabriel.zirkovits@gmail.com>
2023-04-08 20:31:58 +0200

    Bla

--- main.js
+++ main.js
@@ -7,29 +7,41 @@ import { CanvasCapture } from 'canvas-capture';
 /**
  * Capturer
  */
-var capturer = new CCapture({
-  framerate: 60,
-  verbose: true,
-  format: 'webm',
-  display: true,
-  name: 'Magic_Ring',
-  quality: 50,
+CanvasCapture.init(
+  document.querySelector('canvas.webgl'),
+  { showRecDot: true }, // Options are optional, more info below.
+);
+CanvasCapture.bindKeyToVideoRecord('v', {
+  format: 'webm', // Options are optional, more info below.
+  fps: 60,
+  name: 'myVideo',
+  quality: 1.0,
 });
 
 /**
  * Parameters
  */
 const params = {
-  numParticles: 100000,
-  ringRadius: 2,
-  particleSize: 0.03,
-  globalnoiseScale: 0.005,
-  circleThickness: 0.7,
+  numParticles: 70000,
+  ringRadius: 1,
+  particleSize: 0.1,
+  globalnoiseScale: 0.001,
+  circleThickness: 0.6,
   noiseScaleX: 1,
   noiseScaleY: 1,
   noiseScaleZ: 1,
   speedFactor: 1,
   depthWrite: true,
+  zNoiseFactor: 1.0,
+  knot: true,
+  p: 3,
+  q: 2,
+  spreadScale: 1,
+  knotOuterRad: 2,
+  knotInnerRad: 1,
+
+  width: 1080,
+  heigt: 1350,
 }
 
 // const params = {
@@ -42,6 +54,7 @@ const params = {
 //   noiseScaleY: 1,
 //   noiseScaleZ: 1,
 //   speedFactor: 1,
+//   zNoiseFactor: 1.5
 // }
 
 /**
@@ -59,14 +72,12 @@ const canvas = document.getElementById("magic-circle");
  * Scene
  */
 const scene = new THREE.Scene();
-
-/**
+/*
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Handle different pixeldensities
 renderer.setSize(window.innerWidth, window.innerHeight)
@@ -81,8 +92,8 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
  * Orbitcontrols
  */
 const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(2, 2, 8);
-controls.autoRotate = true;
+camera.position.set(0, 1, 8);
+controls.autoRotate = false;
 controls.autoRotateSpeed = 4.0;
 controls.enableDamping = true;
 controls.update();
@@ -103,6 +114,11 @@ let particles = null;
 let particlesGeometry = null;
 let particlesMaterial = null;
 let particlesSpeeds = [];
+let angles = [];
+let randomX = [];
+let randomY = [];
+let randomZ = [];
+let colors = [];
 
 function generateMagicRing() {
   if (particles != null) {
@@ -112,7 +128,7 @@ function generateMagicRing() {
   }
   let vertices = [];
 
-
+  if (!params.knot) {
   for (let i = 0; i < params.numParticles; i++) {
     let i3 = i * 3;
     let randomAngle = THREE.MathUtils.mapLinear(Math.random(), 0, 1, 0, Math.PI * 2);
@@ -122,24 +138,48 @@ function generateMagicRing() {
     vertices[i3 + 1] = randomRadius * Math.cos(randomAngle);
     vertices[i3 + 2] = Math.random();
     particlesSpeeds.push(Math.random() * 0.01);
-  };
+    
+  };}
 
+  else {
+    for (let i = 0; i < params.numParticles; i++) {
+      let i3 = i * 3;
+      let randomAngle = THREE.MathUtils.mapLinear(Math.random(), 0, 1, 0, Math.PI * 2);
+      angles.push(randomAngle);
+      let radius = (2 + Math.cos(params.p * randomAngle ));
+      let randX = Math.random() * params.spreadScale;
+      randomX.push(randX);
+      let randY = Math.random() * params.spreadScale;
+      randomY.push(randY);
+      let randZ = Math.random() * params.spreadScale;
+      randomZ.push(randZ);
+
+      vertices[i3 + 0] = radius * Math.cos(params.q * randomAngle) + randX;
+      vertices[i3 + 1] = radius * Math.sin(params.q * randomAngle) + randY;
+      vertices[i3 + 2] = Math.sin(params.p * randomAngle) + randZ;
+      particlesSpeeds.push(Math.random() * 0.01);
+      
+    };}
+  
   particlesGeometry = new THREE.BufferGeometry();
   particlesGeometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3))
   particlesMaterial = new THREE.PointsMaterial({
-    color: 0xffffff,
+    // color: '0xffffff',
     size: params.particleSize,
     blending: THREE.AdditiveBlending,
     sizeAttenuation: true,
     depthWrite: params.depthWrite,
     transparent: true,
     alphaMap: particleTexture,
+    vertexColors: true,
+    depthTest: true
 
   });
   particles = new THREE.Points(particlesGeometry, particlesMaterial);
   scene.add(particles);
 }
 generateMagicRing();
+
 /**
  * GUI
  */
@@ -147,7 +187,7 @@ const gui = new GUI();
 gui.add(params, 'numParticles').min(100).max(200000).step(10000).name("Particle number").onFinishChange(generateMagicRing);
 gui.add(params, 'ringRadius').min(1).max(10).step(1).name("Ring radius").onFinishChange(generateMagicRing);
 gui.add(params, 'particleSize').min(0.005).max(0.1).step(0.001).name("Particle size").onFinishChange(generateMagicRing);
-gui.add(params, 'globalnoiseScale').min(0.0001).max(0.05).step(0.001).name("Global Noise scale")
+gui.add(params, 'globalnoiseScale').min(0.0001).max(1).step(0.001).name("Global Noise scale")
 gui.add(params, 'circleThickness').min(0.1).max(1).step(0.1).name("Circle thickness").onFinishChange(generateMagicRing);
 gui.add(params, 'noiseScaleX').min(0.1).max(10).step(0.5).name("Noise scale x").onFinishChange(generateMagicRing);
 gui.add(params, 'noiseScaleY').min(0.1).max(10).step(0.5).name("Noise scale Y").onFinishChange(generateMagicRing);
@@ -155,6 +195,12 @@ gui.add(params, 'noiseScaleZ').min(0.1).max(10).step(0.5).name("Noise scale Z").
 gui.add(controls, 'autoRotateSpeed').min(0).max(10).step(0.5).name("Autorotate speed").onFinishChange(generateMagicRing);;
 gui.add(params, 'speedFactor').min(0.1).max(10).step(0.5).name("Speed factor").onFinishChange(generateMagicRing);
 gui.add(params, 'depthWrite').name("Depth write").onFinishChange(generateMagicRing);
+gui.add(params, "zNoiseFactor").min(0.1).max(10).step(0.5).name("z Noise factor").onFinishChange(generateMagicRing);
+gui.add(params, 'q').min(1).max(10).step(1).name("q").onFinishChange(generateMagicRing);
+gui.add(params, 'p').min(1).max(10).step(1).name("p").onFinishChange(generateMagicRing);
+gui.add(params, "spreadScale").min(0.1).max(2).step(0.1).name("Spread scale").onFinishChange(generateMagicRing);
+gui.add(params, "knotOuterRad").min(1).max(3).step(1).name("Knot outer radius").onFinishChange(generateMagicRing);
+gui.add(params, "knotInnerRad").min(0.1).max(2).step(0.1).name("Knot inner radius").onFinishChange(generateMagicRing);
 
 function animate() {
 
@@ -163,27 +209,54 @@ function animate() {
 
   let vertices = [];
   let prevPositions = particles.geometry.attributes.position.array;
-
   for (let i = 0; i < params.numParticles; i++) {
     let i3 = i * 3;
 
     // Noise
     let noise = noise3D(prevPositions[i3 + 0] / params.noiseScaleX, prevPositions[i3 + 1] / params.noiseScaleY, prevPositions[i3 + 2] / params.noiseScaleZ) * params.globalnoiseScale;
-    // let noise = noise4D(prevPositions[i3 + 0], prevPositions[i3 + 1], prevPositions[i3 + 2], Math.random()) * params.globalnoiseScale
-
+    // let noise = noise4D(prevPositions[i3 + 0], prevPositions[i3 + 1], prevPositions[i3 + 2], performance.now()) * params.globalnoiseScale
     let prevAngle = Math.atan2(prevPositions[i3 + 0], prevPositions[i3 + 1]);
-    let particleRadius = Math.sqrt(Math.pow(prevPositions[i3 + 0], 2) + Math.pow(prevPositions[i3 + 1], 2))
+    let particleRadius = Math.sqrt(Math.pow(prevPositions[i3 + 0], 2) + Math.pow(prevPositions[i3 + 1], 2));
+    if (!params.knot) {
+
     vertices[i3 + 0] = particleRadius * Math.sin(prevAngle + particlesSpeeds[i] * params.speedFactor) + noise;
     vertices[i3 + 1] = particleRadius * Math.cos(prevAngle + particlesSpeeds[i] * params.speedFactor) + noise;
-    vertices[i3 + 2] = prevPositions[i3 + 2] + noise * 1.5;
+    vertices[i3 + 2] = prevPositions[i3 + 2] + noise * params.zNoiseFactor;
+    } 
+
+    else {
+      let oldAngle = angles[i];
+
+      let noise = noise3D(randomX[i], randomY[i], randomZ[i]);
+      noise *= params.globalnoiseScale;
+      let newAngle = (oldAngle + 0.002) % (Math.PI * 2);
+      angles[i] = newAngle;
+
+      let radius = (params.knotOuterRad + params.knotInnerRad *Math.cos(params.p * newAngle));
+      vertices[i3 + 0] = (radius * Math.cos(params.q * newAngle )) + randomX[i] * params.spreadScale + noise;
+      vertices[i3 + 1] = (radius * Math.sin(params.q * newAngle )) + randomY[i] * params.spreadScale + noise;
+      vertices[i3 + 2] = Math.sin(params.p * newAngle ) + randomZ[i] * params.spreadScale + noise;
+      
+      let color = new THREE.Color();
+      let hue = THREE.MathUtils.mapLinear(newAngle, 0, Math.PI * 2, 0.3, 0.9);
+      color.setHSL(hue, 1, 0.5);
+      colors[i3 + 0] = color.r;
+      colors[i3 + 1] = color.g;
+      colors[i3 + 2] = color.b;
+     }
   };
-
   particles.geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
-
+  particles.geometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
   particles.geometry.attributes.position.needsUpdate = true;
 
+
   window.requestAnimationFrame(animate);
-  capturer.capture(canvas);
+  CanvasCapture.checkHotkeys();
+
+  // You need to call recordFrame() only if you are recording
+  // a video, gif, or frames.
+  if (CanvasCapture.isRecording()) CanvasCapture.recordFrame();
+
 };
 animate();
 
@@ -200,19 +273,25 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 };
 
-document.body.addEventListener("keydown", function (e) {
-  if (e.key == " " ||
-    e.code == "Space" ||
-    e.keyCode == 32
-  ) {
-    capturer.stop();
-    capturer.save();
+function calcPolarfromCartCoords(x, y, z) {
+  let r = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+  let theta = Math.acos(z / r);
+  let phi = Math.atan2(y, x);
+  return {
+      "radius": r,
+      "theta": theta,
+      "phi": phi
   }
-});
+}
 
-document.body.addEventListener("keydown", function (e) {
-  if (e.key == "s" ||
-    e.code == "KeyS") {
-    capturer.start()
+function calcCartCoordsFromPolar(r, theta, phi) {
+  let x = r * Math.sin(theta) * Math.cos(phi);
+  let y = r * Math.sin(theta) * Math.sin(phi);
+  let z = r * Math.cos(theta);
+
+  return {
+      "newX": x,
+      "newY": y,
+      "newZ": z
   }
-});
+}
